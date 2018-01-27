272f2765517f64849468e780ffe09debae5a764e
MrBBot <me@mrbbot.co.uk>
2018-01-27 15:50:07 +0000

    Cubezzzzzzz

--- Gyro-Branch/c.py
+++ Gyro-Branch/c.py
@@ -1,6 +1,7 @@
 from sr.robot import *
 import time
 
+# Utils
 GYRO_CONST = 0.02  # 0.02
 GYRO_PIN = 1
 GYRO_SAMPLES = 16
@@ -26,12 +27,9 @@ print "Gyro zeroed at:", GYRO_ZERO
 
 def gyro():
     reading = R.gpio.analog_read(GYRO_PIN)
-    print 'G:', reading
+    #print 'G:', reading
     reading -= GYRO_ZERO
     reading /= 8192.0
-    # reading *= 100.0
-    # reading = int(reading)
-    # reading /= 100.0
     return clamp(reading, -1, 1)
 
 
@@ -62,21 +60,11 @@ def power(robot, p):
     robot.motors[0].m0.power = m_left_speed
     robot.motors[0].m1.power = m_right_speed
 
-    print 'L:', m_left_speed, 'R:', m_right_speed
-    print
+    #print 'L:', m_left_speed, 'R:', m_right_speed
+    #print
 
     last_speed_left = m_left_speed
     last_speed_right = m_right_speed
-
-
-def goto_power(robot, target):
-    difference = target - int(last_speed_left)
-    step = (1 if difference > 0 else -1)
-
-    for j in range(int(last_speed_left), target, step):
-        power(robot, j)
-
-        time.sleep(0.02)
 
 
 def stay_at_power(robot, target, total_time):
@@ -88,6 +76,51 @@ def stay_at_power(robot, target, total_time):
         time_elapsed += 0.02
 
 
-stay_at_power(R, 50, 5)
+def spin(robot, total_time):
+    global last_speed_left, last_speed_right
 
-stay_at_power(R, 0, 1)
+    last_speed_left = last_speed_right = 30
+    robot.motors[0].m0.power = 30
+    robot.motors[0].m1.power = 30
+
+    time.sleep(total_time)
+
+    last_speed_left = last_speed_right = 0
+    robot.motors[0].m0.power = 0
+    robot.motors[0].m1.power = 0
+
+
+# Actual Code
+seen_cubes = [34]
+
+while True:
+    print "Seeing"
+    print seen_cubes
+    markers = R.see(res=(640, 480), save=False)
+    if len(markers) > 0:
+        print "Seen", len(markers), "cubes"
+        cubes = 0
+        for marker in markers:
+            if marker.info.code in seen_cubes:
+                print "Skipping", marker.info.code
+                continue
+            elif marker.dist < 0.5:
+                print "Added", marker.info.code, " to the list..."
+                seen_cubes.append(marker.info.code)
+                continue
+            cubes += 1
+            print "Moving towards", marker.info.code, "..."
+            stay_at_power(R, 30, 1)
+        if cubes == 0:
+            spin(R, 1)
+    else:
+        print "Looking for cubes..."
+        spin(R, 1)
+    time.sleep(1)
+
+
+# spin(R, 1)
+#
+# stay_at_power(R, 50, 5)
+#
+# stay_at_power(R, 0, 1)
