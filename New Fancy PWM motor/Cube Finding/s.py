5124f0647e58cb63a3549d29f71b3eae241c7c8b
MrBBot <me@mrbbot.co.uk>
2018-02-10 15:58:11 +0000

    NN

--- New Fancy PWM motor/Cube Finding/s.py
+++ New Fancy PWM motor/Cube Finding/s.py
@@ -12,14 +12,19 @@ GPIO_GATE = 1
 GPIO_PUMP = 2
 
 MULTIPLIER_LEFT = -1
-MULTIPLIER_RIGHT = 0.80
+MULTIPLIER_RIGHT = 0.81
 
 # 100%
 # 3s 2.5m +
 # 2s
 
 # 50%
-# 3s
+# 3s 1.25m
+# (1.25 / 3) m/s
+
+SPEED_50 = 1.25 / 3
+SPEED_100 = 1.7 * SPEED_50 * 1.25
+
 
 R.gpio.pin_mode(GPIO_GATE, OUTPUT)
 R.gpio.digital_write(GPIO_GATE, True)
@@ -27,10 +32,12 @@ R.servos[SERVO_RIGHT] = 0
 R.servos[SERVO_LEFT] = 0
 time.sleep(0.5)
 
-R.servos[SERVO_LEFT] = MULTIPLIER_LEFT * 50
-R.servos[SERVO_RIGHT] = MULTIPLIER_RIGHT * 50
+R.servos[SERVO_LEFT] = MULTIPLIER_LEFT * 33
+R.servos[SERVO_RIGHT] = MULTIPLIER_RIGHT * -33
 
-time.sleep(3)
+# metres / SPEED_100
+
+time.sleep(1)
 
 R.servos[SERVO_LEFT] = 0
 R.servos[SERVO_RIGHT] = 0
