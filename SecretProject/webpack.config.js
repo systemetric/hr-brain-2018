8492cc21241fe034cd0e34b1066d4992a861e3f3
mrbbot <me@mrbbot.co.uk>
2018-02-15 09:42:23 +0000

    Removed gulp in favour of webpack

--- SecretProject/webpack.config.js
+++ SecretProject/webpack.config.js
@@ -1,24 +1,34 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const buildRoot = path.resolve(__dirname, "./js");
-const outputRoot = path.resolve(__dirname, "./js/monaco");
+const jsSrcRoot = path.resolve(__dirname, "./js");
+const jsDistRoot = path.resolve(__dirname, "./js/monaco");
 const monacoEditorPath = './node_modules/monaco-editor-core/dev/vs';
 
 module.exports = {
-    watch: true,
-    entry: path.resolve(buildRoot, "tab-code.js"),
+    entry: ['./js/tab-code.js', './css/main.scss'],
     output: {
         filename: 'bundle.js',
-        path: outputRoot
+        path: jsDistRoot
     },
     module: {
-        noParse: /vscode-languageserver-types/
+        noParse: /vscode-languageserver-types/,
+        rules: [
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract(['css-loader']),
+            },
+            {
+                test: /\.(sass|scss)$/,
+                loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+            }
+        ]
     },
     resolve: {
         extensions: ['.js'],
         alias: {
-            'vs': path.resolve(buildRoot, monacoEditorPath)
+            'vs': path.resolve(jsSrcRoot, monacoEditorPath)
         }
     },
     devtool: 'source-map',
@@ -35,6 +45,10 @@ module.exports = {
                 from: monacoEditorPath,
                 to: 'vs'
             }
-        ])
+        ]),
+        new ExtractTextPlugin({
+            filename: 'css/[name].css',
+            allChunks: true,
+        })
     ]
 };
