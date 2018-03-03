5a209e43ae03efd48f28d93571ddc0333fbeac0e
MrBBot <me@mrbbot.co.uk>
2018-03-03 10:52:42 +0000

    Moved SecretProject to its own repo

--- SecretProject/language-server/setup-env.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-mkdir env
-cd env
-echo Setting up virtual environment...
-virtualenv .
-echo Installing python-language-server...
-Scripts\pip install python-language-server
-echo Setup complete!
--- SecretProject/language-server/setup-env.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-mkdir env
-cd env
-echo Setting up virtual environment...
-virtualenv .
-echo Installing python-language-server...
-Scripts\pip install python-language-server
-echo Setup complete!
